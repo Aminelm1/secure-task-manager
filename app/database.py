9f2e8e6d5281ba01b82d3a6b85fb990b86c6245b
Amine <amineamino030@gmail.com>
2026-09-07 18:43:25 +0100

    Dockerize FastAPI and PostgreSQL services

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://taskuser:taskpassword@localhost:5432/taskdb"
+DATABASE_URL = "postgresql+psycopg://taskuser:taskpassword@postgres:5432/taskdb"
 
 engine = create_engine(DATABASE_URL)
 
