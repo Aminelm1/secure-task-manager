30f0da164a2449e8024b4f75dff412399d1d21c7
Amine <amineamino030@gmail.com>
2026-09-07 18:35:27 +0100

    PostgreSQL  persistence and task CRUD

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+from app.database import Base, engine, SessionLocal
+from app.models import Task
+
 
 app = FastAPI(
     title="Secure Task Manager API",
@@ -6,16 +12,82 @@ app = FastAPI(
     version="1.0.0"
 )
 
+Base.metadata.create_all(bind=engine)
+
+
+class TaskCreate(BaseModel):
+    title: str
+
+
+class TaskUpdate(BaseModel):
+    title: str
+    completed: bool
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def home():
-    return {
-        "message": "Secure Task Manager API is running"
-    }
+    return {"message": "Secure Task Manager API is running"}
 
 
 @app.get("/health")
 def health():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
+
+
+@app.post("/tasks")
+def create_task(task: TaskCreate, db: Session = Depends(get_db)):
+    new_task = Task(
+        title=task.title
+    )
+
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
+    return new_task
+
+
+@app.get("/tasks")
+def get_tasks(db: Session = Depends(get_db)):
+    return db.query(Task).all()
+
+
+@app.put("/tasks/{task_id}")
+def update_task(
+    task_id: int,
+    task: TaskUpdate,
+    db: Session = Depends(get_db)
+):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+
+    if not db_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db_task.title = task.title
+    db_task.completed = task.completed
+
+    db.commit()
+    db.refresh(db_task)
+
+    return db_task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    db_task = db.query(Task).filter(Task.id == task_id).first()
+
+    if not db_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+
+    db.delete(db_task)
+    db.commit()
+
+    return {"message": "Task deleted"}
